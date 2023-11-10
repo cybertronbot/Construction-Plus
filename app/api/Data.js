232326ae4fa187d39fb0712c860f3b43d938259a
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-10 11:46:57 +0100

    fix

--- app/api/Data.js
+++ app/api/Data.js
@@ -6,7 +6,7 @@ import projectfive from "@/public/projecttwo.png";
 import linkedin from "@/public/linkedin.png";
 import sms from "@/public/sms.png";
 import instagram from "@/public/instagram.png";
-import construction from "@/public/construction 1.png"
+import construction from "@/public/construction 1.png";
 
 export const navLinks = [
   {
@@ -27,25 +27,25 @@ export const navLinks = [
   },
 ];
 
-export const  businesslines = [
+export const businesslines = [
   {
     id: "feature-1",
-    img:construction,
-    
+    img: construction,
+
     title: "Infrastructure",
     content:
       "Our journey through infrastructure is an embodiment of our collective technical acumen, honed expertise, and innovative approach. We seamlessly conceive, construct, and maintain critical infrastructure that forms the lifeblood of thriving communities and aligns with the ever-evolving needs of contemporary society. Our dedicated team is immersed in the art of planning, envisioning, and executing vital infrastructure projects that stand as enduring symbols of progress.",
   },
   {
     id: "feature-2",
-    img:construction,
+    img: construction,
     title: "Real Estate",
     content:
       "From conducting exhaustive market analyses and comprehensive feasibility studies to crafting visionary designs, orchestrating precise construction, and mastering property management, we navigate the labyrinthine landscape of real estate with finesse. Our mission is to ensure that every real estate endeavor, from inception to sustainable management, becomes a resounding success.",
   },
   {
     id: "feature-3",
-    img:construction,
+    img: construction,
     title: "Construction",
     content:
       "In construction, Contracting Plus stands as a paragon of expertise. We transform architectural visions into tangible reality with meticulous planning and impeccable execution. Every structure we craft is a living testament to our unwavering commitment to quality and excellence. Our construction projects transcend the ordinary, serving as iconic landmarks that reflect our devotion to the craft of construction.",
@@ -55,75 +55,59 @@ export const  businesslines = [
 export const projects = [
   {
     id: "projectone",
-    img:projectone,
+    img: projectone,
     title: "Project One",
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
+    content: "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
   },
   {
     id: "projecttwo",
     img: projecttwo,
     title: "Project Two",
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
+    content: "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
   },
   {
     id: "projectthree",
     img: projectfour,
     title: "Project Three",
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
+    content: "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
   },
   {
     id: "projectfive",
     img: projectthree,
     title: "Project Four",
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
+    content: "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
   },
   {
     id: "projectfive",
     img: projectfive,
     title: "Project Five",
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
+    content: "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
   },
   {
     id: "projectfive",
     img: projectfive,
     title: "Project One",
-    content:
-      "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
+    content: "Lorem ipsum dolor sit amet consectetur. Et nec tellus risus vit",
   },
 ];
-
-
-
-
 
 export const footerLinks = [
   {
     title: "Address",
     links: [
       {
+        id: "1",
         name: "12 Igbodo Street, Old GRA, Port Harcourt, River State, Nigeria",
         link: "#",
       },
-      
     ],
   },
 
   {
     title: "Partner",
     links: [
-      {
-        name: "Privacy Policy",
-        link: "/",
-      },
-      {
-        name: "Terms and conditions",
-        link: "/",
-      },
+      { id: "1", name: "Privacy Policy", link: "/" },
+      { id: "2", name: "Terms and conditions", link: "/" },
     ],
   },
 ];
@@ -144,5 +128,4 @@ export const socialMedia = [
     icon: instagram,
     link: "https://www.twitter.com/",
   },
-
 ];
