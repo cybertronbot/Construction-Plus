638660ca6309e85be50767effe00cdfda3f1dd34
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-10 12:47:45 +0100

    commit

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -9,9 +9,9 @@ const Hero = () => {
           backgroundImage: `url("/background.png")`, 
         }}
       >
-       <div className='px-20'>
-       <h4 className='font-Agency relative v flex items-center pl-20 text-base font-bold text-primary'>C O N S T R U C T I O N B U S I N E S S</h4>
-        <h2 className='text-6xl leading-[72px] font-Agency -translate-x-[2px] font-bold text-white'>Crafting Excellence, One <br/>Project at a Time. Your <br/>Trusted Construction Partner.</h2>
+       <div className='px-5 sm:px-20'>
+       <h4 className='font-Agency relative v flex items-center sm:pl-20 pl-14 text-base font-bold text-primary'>C O N S T R U C T I O N B U S I N E S S</h4>
+        <h2 className='sm:text-6xl text-5xl sm:leading-[72px] font-Agency -translate-x-[2px] font-bold text-white'>Crafting Excellence, One <br className='sm:flex hidden'/>Project at a Time. Your <br className='sm:flex hidden'/>Trusted Construction Partner.</h2>
        
        </div>
 
