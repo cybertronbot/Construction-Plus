02cc72e116c8ed7c05a573793008bf91b955b318
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-10 13:36:23 +0100

    commit

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,10 +1,14 @@
+"use client"
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/logo.png";
 import send from "@/public/send.png";
+
 import { Button } from "./buttons/Button";
 import { navLinks } from "../api/Data";
+import { RiMenu3Fill } from "react-icons/ri";
+import { IconContext } from "react-icons/lib";
 const Navbar = () => {
   return (
     <div>
@@ -14,7 +18,7 @@ const Navbar = () => {
             <Image src={logo} alt="logo" layout="fill" objectFit="cover" />
           </div>
         </Link>
-        <div className="w-[200px] h-[40px]"/>
+        <div className="w-[50px] h-[40px]"/>
         <nav className="sm:flex hidden">
           <ul className="flex text-[18px] gap-8 font-bold font-Agency  ">
             {navLinks.map((navlink, index) => {
@@ -22,10 +26,15 @@ const Navbar = () => {
             })}
           </ul>
         </nav>
-        <div className="">
+        <div className="flex items-center gap-3">
           <Button variant="primary" imgSrc={send} imgAlt="send">
             Get A Quote
           </Button>
+          <IconContext.Provider value={{size:"25px",color:"red"}}>
+          <RiMenu3Fill/>
+          </IconContext.Provider>
+          
+         
         </div>
       </div>
     </div>
