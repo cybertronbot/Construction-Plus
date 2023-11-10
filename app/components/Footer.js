d1dfe8bc8589566276033a26a783be1d427cfc8f
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-10 11:52:19 +0100

    fix

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -7,56 +7,66 @@ import Image from "next/image";
 function Footer() {
   return (
     <div className="bg-[#161616] ">
+      <div className="flex justify-center items-center flex-col ">
+        <div className="flex justify-center items-start md:flex-row flex-col w-full  px-20 pt-16 pr-[300px]">
+          <div className="flex-1 flex flex-col justify-start mr-10">
+            <Link href="/">
+              <Image src={footerlogo} width={204} height={44} alt="logo" />
+            </Link>
 
-   
-    <div className="flex justify-center items-center flex-col ">
-      <div className="flex justify-center items-start md:flex-row flex-col w-full  px-20 pt-16 pr-[300px]">
-        <div className="flex-1 flex flex-col justify-start mr-10">
-          <Link href="/">
-            <Image src={footerlogo} width={204} height={44} alt="logo" />
-          </Link>
-
-          <p className="font-poppins font-medium  text-white text-sm mt-4 max-w-[410px]">
-            <span className="text-primary">Email Address: </span>
-            info@contractingplus.com
-          </p>
-          <p className="font-poppins font-medium  text-white text-sm mt-3 max-w-[410px]">
-            <span>Telephone: </span>+234 905 6161 616
-          </p>
-        </div>
-        <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap ">
-          {footerLinks.map((item,index) => (
-            <div className="flex flex-col  min-w-[150px]" key={index}>
-              <h4 className="font-poppins font-medium text-[14px] uppercase text-primary">
-                {item.title}
-              </h4>
-              <ul className="mt-4">
-                {item.links.map((item,index) => {
-                  return (
-                    <li className="font-poppins font-medium text-[16px] text-white max-w-[310px] " key={index}>
-                      {item.name}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          ))}
+            <p className="font-poppins font-medium  text-white text-sm mt-4 max-w-[410px]">
+              <span className="text-primary">Email Address: </span>
+              info@contractingplus.com
+            </p>
+            <p className="font-poppins font-medium  text-white text-sm mt-3 max-w-[410px]">
+              <span>Telephone: </span>+234 905 6161 616
+            </p>
+          </div>
+          <ul className="flex-[1.5] w-full flex flex-row justify-between flex-wrap ">
+            {footerLinks.map((footerLink, index) => (
+              <li className="flex flex-col  min-w-[150px]" key={index}>
+                <h4 className="font-poppins font-medium text-[14px] uppercase text-primary">
+                  {footerLink.title}
+                </h4>
+                <ul className="mt-4">
+                  {footerLink.links.map((link, index) => {
+                    return (
+                      <li
+                        className="font-poppins font-medium text-[16px] text-white max-w-[310px] "
+                        key={index}
+                      >
+                        {link.name}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-      
-    </div>
-    <div className="flex gap-3 justify-end px-32">
-          {socialMedia.map((item,index)=>{
-            return(
-              <div className="w-[50px] h-[50px] flex justify-center items-center rounded-full  mb-8 border border-[#ffff]" key={index}>
-                <Image width={16} height={16} src={item.icon} alt="social media" />
-              </div>
-            )
-          })}
-        </div>
+
+      <ul className="flex gap-3 justify-end px-32">
+        {socialMedia.map((social, index) => {
+          return (
+            <li
+              className="w-[50px] h-[50px] flex justify-center items-center rounded-full  mb-8 border border-[#ffff]"
+              key={index}
+            >
+              <Image
+                width={16}
+                height={16}
+                src={social.icon}
+                alt="social media"
+              />
+            </li>
+          );
+        })}
+      </ul>
       <div className="pt-6 border-t-[1px] border-t-[#3F3E45] mx-20 ">
-        <p className="font-poppins text-sm leading-[27px] text-white text-center">Construction Plus Service Limited © 2023 All rights reserved.</p>
-       
+        <p className="font-poppins text-sm leading-[27px] text-white text-center">
+          Construction Plus Service Limited © 2023 All rights reserved.
+        </p>
       </div>
     </div>
   );
